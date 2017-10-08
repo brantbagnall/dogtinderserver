63060b9bb371ef641f7f9f8cd39ae98ebbe0aa25
Brant Bagnall <brant.bagnall@gmail.com>
2017-10-08 14:15:18 -0600

    finished all mvp functionality

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -6,15 +6,19 @@ const axios = require('axios'),
     db = low(adapter);
 
     db.defaults({ 
-        currentUser: 'none',
+        currentUser: 'default',
+        currentId: 0,
         dog: {
             breed: 'none',
             link: 'none',
-            user: 'none'
+            user: 'default',
+            id: 0
         },
+        like: [],
+        dislike: [],
 
      })
-    .write()
+    .write();
 
 module.exports = {
     getAllRandom: function (req, res){
@@ -26,7 +30,9 @@ module.exports = {
             }
             db.set('dog.breed', breed).write();
             db.set('dog.link', e.data.message).write();
-            db.set('dog.user', db.get('currentUser').value())
+            db.set('dog.user', db.get('currentUser').value()).write()
+            db.set('dog.id', db.get('currentId').value()).write()
+            db.set('currentId', db.get('currentId').value() + 1).write()
             return res.status(200).send(db.get('dog'));
         })     
     },
@@ -35,7 +41,9 @@ module.exports = {
             
             db.set('dog.breed', req.params.breed).write();
             db.set('dog.link', e.data.message).write();
-            db.set('dog.user', db.get('currentUser').value())
+            db.set('dog.user', db.get('currentUser').value()).write()
+            db.set('dog.id', db.get('currentId').value()).write()
+            db.set('currentId',db.get('currentId').value() + 1).write()
             return res.status(200).send(db.get('dog'))
         });
     },
@@ -43,14 +51,47 @@ module.exports = {
         axios.get(`${apiUrl}/breed/${req.params.breed}/${req.params.sub}/images/random`).then(e=> {
             db.set('dog.breed', req.params.breed).write();
             db.set('dog.link', e.data.message).write();
-            db.set('dog.user', db.get('currentUser').value())
+            db.set('dog.user', db.get('currentUser').value()).write()
+            db.set('dog.id', db.get('currentId').value()).write()
+            db.set('currentId', db.get('currentId').value() + 1).write()
             return res.status(200).send(db.get('dog'))
     });
     },
-    postLike: function (req, res) {
-        
+    postList: function (req, res) {
+        if (req.body.list === 'like') {
+            var test = Object.assign({},db.get('dog').value());
+            db.get('like').push(test).write();
+            res.status(200).send(db.get('like'));
+        } else {
+            var test = Object.assign({},db.get('dog').value());
+            db.get('dislike').push(test).write();
+            res.status(200).send(db.get('dislike'));
+        }
     },
-    postDislike: function (req, res) {
-        
+    deleteById: function (req, res) {
+        var deleteId =  req.params.id;
+        var list = req.params.list;
+        // console.log(deleteId);
+        var dbLook = db.get(list).value();
+        dbLook.map((curr, i, arr) => {
+            if(curr.id == deleteId){
+                arr.splice(i, 1);
+            }
+        })
+        db.set(list, Object.assign({}, dbLook));
+        return res.status(200).send(db.get(list));
+    },
+    breedList: function(req, res) {
+        axios.get(apiUrl + '/breeds/list').then(e => res.status(200).send(e.data.message));
+    },
+    subList: function(req, res){
+        axios.get(apiUrl + `/breed/${req.params.breed}/list`).then(e => res.status(200).send(e.data.message));
+    },
+    getLike: function(req, res){
+        res.status(200).send(db.get('like').value())
+    },
+    getDislike: function(req, res) {
+        res.status(200).send(db.get('dislike').value())
     }
+    
 }
--- server/index.js
+++ server/index.js
@@ -2,17 +2,32 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     axios = require('axios'),
     ctrl = require('./controllers/controller.js'),
+    cors = require('cors'),
     app = express(),
     port = 3001;
 
     app.use(bodyParser.json());
 
-    app.use(express.static('./../../dogtinderclient/public'));
+    app.use(cors());
+
+    app.use(express.static('./../../dogtinderclient/build'));
+      
+    app.get('/api/breedlist', ctrl.breedList);
+
+    app.get('/api/like', ctrl.getLike);
+
+    app.get('/api/dislike', ctrl.getDislike);
+
+    app.get('/api/subList/:breed', ctrl.subList);
 
     app.get('/api/all', ctrl.getAllRandom);
 
     app.get('/api/:breed', ctrl.getByBreed);
 
-    app.get('/api/:breed/:sub', ctrl.getBySubBreed)
+    app.get('/api/:breed/:sub', ctrl.getBySubBreed);
+
+    app.post('/api/list', ctrl.postList);
+
+    app.delete('/api/delete/:list/:id', ctrl.deleteById);
 
 app.listen(port, ()=> console.log(`listening on port ${port}`));
